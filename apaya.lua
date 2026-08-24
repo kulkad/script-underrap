6bdc12253de9688f3f612309a12ac6e3a07b8b62
Rizky Darmawan <48381346+kulkad@users.noreply.github.com>
2026-08-24 21:45:05 +0700

    nambahin boosted list

--- apaya.lua
+++ apaya.lua
@@ -75,6 +75,7 @@ local BOOSTED_ITEMS = {
     ["Ranked Season 20 Champion"] = true,
     ["Dual Sakura Fan"] = true,
     ["Frog"] = true,
+    ["Dual Aurum Etherius"] = true,
     ["Inferno Greatscythe"] = true,
     ["Awakened Winter's Touch"] = true,
     ["Fire Dragon"] = true,
